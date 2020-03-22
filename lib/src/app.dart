5a351806f91464b62d0686da16e04cf4e8d411cd
Ardin Biganiro <ardinbig@gmail.com>
2020-03-22 15:14:35 +0200

    Project architecture creation

---
+++ lib/src/app.dart
@@ -0,0 +1,12 @@
+import 'package:flutter/material.dart';
+
+class VitabuApp extends StatelessWidget {
+  const VitabuApp({Key key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      child: Center(),
+    );
+  }
+}
