ddc96c8ef8a852fd9c073108374dd98a7849d33c
Ardin Biganiro <ardinbig@gmail.com>
2020-10-11 01:37:56 +0200

    Basique ui remise

--- lib/src/views/screens/acquisition.dart
+++ lib/src/views/screens/acquisition.dart
@@ -1,6 +1,31 @@
 import 'package:flutter/material.dart';
 
 class AcquisitionScreen extends StatelessWidget {
+  final List _numberList = ["Number 1", "Number 2", "Number 3", "Number 4"];
+
+  List<DropdownMenuItem<String>> _dropDownMenuItems;
+
+  String _currentNumber;
+
+  List<DropdownMenuItem<String>> getDropDownMenuItems() {
+    List<DropdownMenuItem<String>> items = List();
+    for (String number in _numberList) {
+      items.add(
+        DropdownMenuItem(
+          value: number,
+          child: Text(number),
+        ),
+      );
+    }
+    return items;
+  }
+
+  void _changedDropDownItem(String selectedNumber) {
+    setState(() {
+      _currentNumber = selectedNumber;
+    });
+  }
+
   @override
   Widget build(BuildContext context) {
     return SafeArea(
@@ -19,11 +44,74 @@ class AcquisitionScreen extends StatelessWidget {
             ),
           ],
         ),
-        body: Center(
-          child: Container(
-            child: Text("ACQUISITION"),
+        body: Container(
+          child: ListView(
+            padding: const EdgeInsets.fromLTRB(30, 30, 30, 10),
+            children: [
+              SizedBox(
+                child: buildTextBox(
+                  "Code RFID",
+                ),
+              ),
+              SizedBox(height: 16),
+              SizedBox(
+                child: buildTextBox(
+                  "Titre",
+                ),
+              ),
+              SizedBox(height: 16),
+              Container(
+                padding: const EdgeInsets.symmetric(horizontal: 10.0),
+                decoration: BoxDecoration(
+                  borderRadius: BorderRadius.circular(5.0),
+                  border: Border.all(
+                    color: Colors.black54,
+                    style: BorderStyle.solid,
+                    width: 0.80,
+                  ),
+                ),
+                child: DropdownButton(
+                  disabledHint: const Text("Auteur"),
+                  isExpanded: true,
+                  value: _currentNumber,
+                  items: _dropDownMenuItems,
+                  onChanged: _changedDropDownItem,
+                ),
+              ),
+              SizedBox(height: 16),
+              Row(
+                mainAxisAlignment: MainAxisAlignment.spaceAround,
+                children: [
+                  Expanded(
+                    flex: 4,
+                    child: buildTextBox(
+                      "Publication",
+                    ),
+                  ),
+                  SizedBox(width: 10),
+                  Expanded(
+                    flex: 3,
+                    child: buildTextBox(
+                      "Version",
+                    ),
+                  ),
+                  SizedBox(width: 10),
+                  Expanded(
+                    flex: 3,
+                    child: buildTextBox(
+                      "Page",
+                    ),
+                  ),
+                ],
+              ),
+              SizedBox(height: 40),
+              Button(
+                caption: "VALIDER",
+                onPressed: () {},
+              ),
+            ],
           ),
-        ),
+        );
       ),
     );
   }
