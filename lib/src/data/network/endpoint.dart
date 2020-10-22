34fea5a576db18fe20961f509c29e82ab02aefd1
Ardin Biganiro <ardinbig@gmail.com>
2020-10-22 10:51:18 +0200

    Ajout des classes endpoint & networkException

---
+++ lib/src/data/network/endpoint.dart
@@ -0,0 +1,11 @@
+class EndPoint {
+  EndPoint._();
+
+  static const String _baseUrl = "http://127.0.0.1:9000/api";
+
+  static const String getListAbonne = _baseUrl + "/emprunt";
+  static const String getListAuteur = _baseUrl + "/auteur";
+  static const String getListType = _baseUrl + "/type";
+  static const String getListEditeur = _baseUrl + "/editeur";
+  static const String getListClasse = _baseUrl + "/classe";
+}
